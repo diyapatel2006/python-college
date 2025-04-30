8b8cd8c7c01a9c107c80a768d884672d395a462f
diyapatel2006 <pdiya302006@gmail.com>
2025-05-01 00:04:13 +0530

    Delete oop.py

--- oop.py
+++
@@ -1,5 +0,0 @@
-#create a list of strings.Find the strings having'a' and store them in list LS.
-def lst():
-    lst=["This","Is","PDPU","College"]
-    x=[str for str in lst if "e" in str]
-print(lst)    
