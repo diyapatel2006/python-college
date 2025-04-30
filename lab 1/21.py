f68a2d39794c2a9d47be17515dd7a52755aa44b7
diyapatel2006 <pdiya302006@gmail.com>
2025-05-01 00:10:20 +0530

    Add files via upload

---
+++ lab 1/21.py
@@ -0,0 +1,3 @@
+minutes=int(input("please enter minutes:"))
+hours=minutes/60
+print(hours,"hours")
---
+++ lab 1/22.py
@@ -0,0 +1,5 @@
+#multiply two numbers
+a=int(input('enter first number:'))
+b=int(input('enter second number:'))
+c=a*b
+print(c)
