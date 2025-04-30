a54d0faecd74fe2d95bf25b3ce6f24ceb5633f31
diyapatel2006 <pdiya302006@gmail.com>
2025-04-30 23:51:48 +0530

    Delete reduce.py

--- reduce.py
+++
@@ -1,8 +0,0 @@
-#find max of list using reduce
-from functools import reduce
-
-r=reduce(lambda p,q:p*q,[x for x in range(1,5)])
-print(r)
-
-
-print(list(filter(lambda s:str(s) == str(s)[::-1],['madam','python',"malayalam",12321])))
