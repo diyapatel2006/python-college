6f3e2b510889ad3cdb013ba81682ca7e4d870b85
diyapatel2006 <pdiya302006@gmail.com>
2025-05-01 00:07:49 +0530

    Delete functions 9(a) directory

--- functions 9(a)/div by 10.py
+++
@@ -1,7 +0,0 @@
-#create a function which will return true if the number is divisible by 10 otherwise false
-def divby10(n):
-    return True if n%10==0 else False
-lst=[5,56,10,90,1010]
-print(list(filter(divby10,lst)))
-print(list(filter(lambda x:True if x%10==0 else False,lst)))
-print(list(filter(lambda x:x%10==0,[5,56,10,90,1010])))
--- functions 9(a)/reduce.py
+++
@@ -1,5 +0,0 @@
-#find max of list using reduce
-from functools import reduce
-
-r=reduce(lambda p,q:p*q,[x for x in range(1,5)])
-print(r)
